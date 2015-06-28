92e458f3a5bb6d8a62b12a1065daf51de42b0f2b
Yusuf Caglar Akyuz <caglar@bilkon-kontrol.com.tr>
2015-06-29 02:03:35 +0300

    main: Override output function.
    This is necessary for some platforms when building with Qt5 support.
    Otherwise QtCreator doesn't show debug messages.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,33 @@
 #include "mainwindow.h"
+
 #include <QApplication>
+
+#include <stdio.h>
+
+static void myMessageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+	QByteArray localMsg = msg.toLocal8Bit();
+	fprintf(stderr, "%s\n", localMsg.constData());
+	return;
+	switch (type) {
+	case QtDebugMsg:
+		fprintf(stderr, "Debug: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+		break;
+	case QtWarningMsg:
+		fprintf(stderr, "Warning: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+		break;
+	case QtCriticalMsg:
+		fprintf(stderr, "Critical: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+		break;
+	case QtFatalMsg:
+		fprintf(stderr, "Fatal: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+		abort();
+	}
+}
 
 int main(int argc, char *argv[])
 {
+	qInstallMessageHandler(myMessageOutput);
 	QApplication a(argc, argv);
 	MainWindow w;
 	w.show();
