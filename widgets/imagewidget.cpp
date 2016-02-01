cad5d0140cea63607da6612cea0da5298e766ad5
Yusuf Caglar Akyuz <caglar@bilkon-kontrol.com.tr>
2016-02-01 16:03:04 +0200

    widgets: Imagewidget: Add colormapped image drawing support.

--- widgets/imagewidget.cpp
+++ widgets/imagewidget.cpp
@@ -4,6 +4,40 @@
 #include <QPainter>
 #include <QFontMetrics>
 
+static double interpolate( double val, double y0, double x0, double y1, double x1 ) {
+	return (val-x0)*(y1-y0)/(x1-x0) + y0;
+}
+
+static double base( double val ) {
+	if ( val <= -0.75 ) return 0;
+	else if ( val <= -0.25 ) return interpolate( val, 0.0, -0.75, 1.0, -0.25 );
+	else if ( val <= 0.25 ) return 1.0;
+	else if ( val <= 0.75 ) return interpolate( val, 1.0, 0.25, 0.0, 0.75 );
+	else return 0.0;
+}
+
+static double red( double gray ) {
+	return base( gray - 0.5 );
+}
+static double green( double gray ) {
+	return base( gray );
+}
+static double blue( double gray ) {
+	return base( gray + 0.5 );
+}
+
+static QImage createIndexed(int w, int h)
+{
+	QImage im(w, h, QImage::Format_Indexed8);
+	QVector<QRgb> jet;
+	for (int i = 0; i < 256; i++) {
+		double ival = i / 128.0 - 1;
+		jet << qRgb(red(ival) * 255, green(ival) * 255, blue(ival) * 255);
+	}
+	im.setColorTable(jet);
+	return im;
+}
+
 class ImageWidgetGridItem {
 public:
 	enum ScaleMode {
@@ -21,12 +55,28 @@ public:
 	}
 	void showMat(const Mat &image)
 	{
-		im = QImage((const uchar *)image.data, image.cols,  image.rows, QImage::Format_RGB888);
+		if (image.type() == CV_32F) {
+			double min, max;
+			minMaxLoc(image, &min, &max);
+			Mat image2 = image.clone();
+			image2 -= min;
+			image2 /= max;
+			im = createIndexed(image.cols, image.rows);
+			for (int i = 0; i < image2.cols; i++) {
+				for (int j = 0; j < image2.rows; j++) {
+					im.setPixel(i, j, image2.at<float>(j, i) * 255);
+				}
+			}
+		} else
+			im = QImage((const uchar *)image.data, image.cols,  image.rows, QImage::Format_RGB888);
 		imgSource = image;
 	}
 	void draw(QPainter *p, QRect r)
 	{
-		p->drawImage(r, im);
+		p->setPen(Qt::red);
+		p->setBrush(QBrush(Qt::red));
+		p->drawRect(r);
+		p->drawImage(QRect(r.left() + 1, r.top() + 1, r.right() - 1, r.bottom() - 1), im);
 
 		/* draw OSD */
 		if (osd.size()) {
